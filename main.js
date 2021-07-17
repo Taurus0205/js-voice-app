29e0c56add4ec02b4ad874ddd63ca797ca65a2fe
Taurus0205 <dolchin1990@mail.ru>
2021-07-17 18:45:09 +0500

    Add speech recorder js

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
-const elHumbergerBtn = document.querySelector(".hamburger-btn");
-const elHeader = document.querySelector(".header");
+var elSpan = document.querySelector(".span");
 
-elHumbergerBtn.addEventListener("click", function () {
-  elHeader.classList.toggle("header--adoptive");
+var rec = new webkitSpeechRecognition();
+rec.lang = "en-US";
+rec.onerror = function (err) {
+  console.log(err, "error");
+};
+
+console.log(rec);
+
+rec.onend = function () {
+  console.log("End of speech");
+};
+
+rec.onresult = function (evt) {
+  var command = evt.results[0][0].transcript;
+  elSpan.textContent = command;
+
+  console.log(command);
+};
+
+button.addEventListener("click", function () {
+  rec.start();
 });
