9120c87d7124eb42b0a89451f21f7fdfd329e7be
Taurus0205 <dolchin1990@mail.ru>
2021-07-18 07:48:30 +0500

    Add mustafo and afruza styles in scss and js

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
 var elSpan = document.querySelector(".span");
+var elImg = document.querySelector(".content-img");
 
 var rec = new webkitSpeechRecognition();
-rec.lang = "en-US";
+rec.lang = "uz-UZ";
 rec.onerror = function (err) {
-  console.log(err, "error");
+  elSpan.textContent = "error";
 };
-
-console.log(rec);
 
 rec.onend = function () {
   console.log("End of speech");
@@ -16,7 +15,11 @@ rec.onresult = function (evt) {
   var command = evt.results[0][0].transcript;
   elSpan.textContent = command;
 
-  console.log(command);
+  if (command === "mustafo") {
+    elImg.classList.add("mustafo");
+  } else if (command === "afruza") {
+    elImg.classList.add("afruza");
+  }
 };
 
 button.addEventListener("click", function () {
